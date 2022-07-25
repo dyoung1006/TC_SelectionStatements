531095085f220776474bd51212a2867e7658e9b2
Damien Young <dyoung1006@gmail.com>
2022-07-24 18:55:06 -0700

    selection statements exercise

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            var r = new Random();
+            int favNumber = r.Next(1, 9);
+            Console.WriteLine("Guess the favorite number. Hint: Below 10");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("Winner Winner Chicken Dinner");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
+    
---
+++ Switch/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your favorite school subject ?");
+            string favSubject = Console.ReadLine();
+
+
+            switch (favSubject)
+            {
+                case "Math":
+                    Console.WriteLine("how many bands we dropping in the strip club?");
+                    break;
+                case "Science":
+                    Console.WriteLine("Breaking Bad?");
+                    break;
+                case "Gym":
+                    Console.WriteLine("Jocks sucks!");
+                    break;
+                case "Music":
+                    Console.WriteLine("Look, if you had one shot or one opportunity, to seize everything..name that song");
+                    break;
+                case "Home Economics":
+                    Console.WriteLine("Cook on family!");
+                    break;
+                default:
+                    Console.WriteLine(@"well... that's not on my list ¯\_(ツ)_/¯");
+                    break;
+
+            }
+
+        }
+    }
+}
